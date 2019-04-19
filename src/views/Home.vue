1a5c0fef32f8f753e3fe81ec520d6da31e36c49c
EmmanuelSW <edhgt89@hotmail.com>
2019-04-18 23:15:37 -0500

    Some styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="logo">
+      <img alt="imgur logo" src="../assets/imgur.jpg">
+      <h1>Imgur Public Profile</h1>
+    </div>
+    <div class="input">
+      <a-form layout="inline">
+        <a-form-item>
+          <a-input v-model="username" class="username" size="large" placeholder="Search by username">
+            <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-button @click="search" size="large" type="primary" :loading="loading" :disabled="!username">
+            Search
+          </a-button>
+        </a-form-item>
+      </a-form>
+    </div>
   </div>
+
 </template>
 
 <script>
-
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      loading: false,
+      username: ''
+    }
+  },
+  methods: {
+    search() {
+      this.loading = true
+    }
   }
 }
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Nunito:700');
+.logo {
+  padding-top: 100px; 
+}
+.logo img {
+  width: 150px;
+}
+.logo h1 {
+  font-family: 'Nunito', sans-serif;
+  font-size: 36px;
+  padding-top: 10px;
+  color: #434343;
+}
+.input {
+  padding-top: 80px;
+}
+.username {
+  width: 400px !important;
+}
+</style>
+
